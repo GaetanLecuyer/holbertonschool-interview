d877f282d9dbab3fddde6d77508fd4ac37c59a0a
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2024-01-16 08:39:49 +0100

    push test

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+def can_unlock_all(boxes):
+    """
+    Détermine si toutes les boîtes peuvent être ouvertes en utilisant les clés présentes dans les boîtes.
+
+    :param boxes: Une liste de listes représentant les boîtes et les clés.
+    :return: True si toutes les boîtes peuvent être ouvertes, False sinon.
+    """
+
+    # Créer une liste pour suivre l'état d'ouverture des boîtes
+    # True signifie que la boîte est ouverte, False signifie qu'elle est fermée
+    opened_boxes = [False] * len(boxes)
+    
+    opened_boxes[0] = True
+    
+    stack = [0]
+    
+    while stack:
+        current_box = stack.pop()
+        
+        for key in boxes[current_box]:
+            if not opened_boxes[key]:
+                opened_boxes[key] = True
+                stack.append(key)
+
+    return all(opened_boxes)
