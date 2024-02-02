0862d07dd98940fd7f691a82e8967aab53ed378c
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2024-02-02 15:32:34 +0100

    test

--- minimum_operations/0-minoperations-strict_primes.py
+++ minimum_operations/0-minoperations-strict_primes.py
@@ -7,18 +7,6 @@ only operations are Copy All and Paste
 
 
 def minOperations(n):
-    """
-    calculates the fewest number of operations needed
-    to result in exactly n copies of a character in a text file,
-    whose only operations are Copy All and Paste
-
-    parameters:
-        n [int]: number of copies of the character desired
-
-    returns:
-        the minimum number of operations needed to result in n characters
-        or 0 if n is impossible to achieve
-    """
     if type(n) is not int or n <= 1:
         return 0
     primes = [2, 3]
